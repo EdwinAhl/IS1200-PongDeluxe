f9288f2450ff510d62f850d05cf58011a242ab8a
BaltzarL <106113033+BaltzarL@users.noreply.github.com>
2022-02-24 12:05:09 +0100

    revert display_image

--- mipslabfunc.c
+++ mipslabfunc.c
@@ -141,24 +141,23 @@ void display_string(int line, char *s) {
 			textbuffer[line][i] = ' ';
 }
 
-void display_image(const uint8_t *data) {
-	int i, j, x;
-	for (x = 0; x < 4; x++){
-    for(i = 0; i < 4; i++) {
-      DISPLAY_CHANGE_TO_COMMAND_MODE;
+void display_image(int x, const uint8_t *data) {
+	int i, j;
+	
+	for(i = 0; i < 4; i++) {
+		DISPLAY_CHANGE_TO_COMMAND_MODE;
 
-      spi_send_recv(0x22);
-      spi_send_recv(i);
-      
-      spi_send_recv(x & 0xF);
-      spi_send_recv(0x10 | ((x >> 4) & 0xF));
-      
-      DISPLAY_CHANGE_TO_DATA_MODE;
-      
-      for(j = 0; j < 32; j++)
-        spi_send_recv(data[i*32 + j + 128*x]);
-    }
-  }
+		spi_send_recv(0x22);
+		spi_send_recv(i);
+		
+		spi_send_recv(x & 0xF);
+		spi_send_recv(0x10 | ((x >> 4) & 0xF));
+		
+		DISPLAY_CHANGE_TO_DATA_MODE;
+		
+		for(j = 0; j < 32; j++)
+			spi_send_recv(data[i*32 + j]);
+	}
 }
 
 void display_update(void) {
